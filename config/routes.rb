1bb7de33f7e778c37b0f85196ddee78051c84d69
Mark Johnson <luther07@gmail.com>
2012-07-21 15:49:11 -0500

    remove merge artifacts from routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 SampleApp::Application.routes.draw do
-<<<<<<< HEAD
-=======
   get "static_pages/home"
 
   get "static_pages/help"
 
   get "static_pages/about"
->>>>>>> static-pages
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
