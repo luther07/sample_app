c85029a190ad57c1f8a58eb01f10d25006204a57
Mark Johnson <luther07@gmail.com>
2012-08-07 07:55:39 -0500

    change routes in order to created named routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
+  root to: 'static_pages#home'
 
-  get "static_pages/help"
-
-  get "static_pages/about"
-
-  get "static_pages/contact"
-
-  get "static_pages/dhh"
-
-  get "static_pages/ueber"
+  match '/help',    to: 'static_pages#help' 
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
+  match '/dhh',     to: 'static_pages#dhh'
+  match '/ueber',   to: 'static_pages#ueber'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
