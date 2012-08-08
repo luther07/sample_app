fbff7fb8e3745b1df1163711f48acabc9392e847
Mark Johnson <luther07@gmail.com>
2012-08-08 13:34:13 -0500

    add rspec utility function, full_title, which parellels a function in applicationhelper module

---
+++ spec/support/utilities.rb
@@ -0,0 +1,9 @@
+# method parallels the full_title 
+def full_title(page_title)
+  base_title = "Ruby on Rails Tutorial Sample App"
+  if page_title.empty?
+    base_title
+  else
+    "#{base_title} | #{page_title}"
+  end
+end
