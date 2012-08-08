6281e75d810b208f30fa94133e0d68a2a24773dd
Mark Johnson <luther07@gmail.com>
2012-08-07 20:44:44 -0500

    eliminate duplication from Home page test, making page the subject of the test and collapsing code with it method

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,20 +4,14 @@ describe "Static pages" do
 
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
+  subject { page }
+
   describe "Home page" do
     before { visit root_path }
 
-    it "should have the h1 'Sample App'" do
-      page.should have_selector('h1', text: 'Sample App')
-    end
-
-    it "should have the base title" do
-      page.should have_selector('title', text: "#{base_title}")
-    end
-
-    it "should not have a custom page title" do
-      page.should_not have_selector('title', text: "#{base_title} | Home")
-    end
+    it { should have_selector('h1', text: 'Sample App') }
+    it { should have_selector('title', text: "#{base_title}") }
+    it { should_not have_selector('title', text: "#{base_title} | Home") }
   end
 
   describe "Help page" do
