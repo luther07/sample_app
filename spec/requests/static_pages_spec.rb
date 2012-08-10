3fa1defe22c3e0f37f87696446239e3ef6f603f9
Mark Johnson <luther07@gmail.com>
2012-08-10 12:58:10 -0500

    convert static pages spec tests to use shared examples, eliminate some duplication

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,39 +6,49 @@ describe "Static pages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1',    text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
+
   describe "Home page" do
     before { visit root_path }
+    let(:heading) { 'Sample App' }
+    let(:page_title) { '' }
 
-    it { should have_selector('h1', text: 'Sample App') }
-    it { should have_selector('title', text: full_title('')) }
+    it_should_behave_like "all static pages"
     it { should_not have_selector('title', text: '| Home') }
   end
 
   describe "Help page" do
     before { visit help_path }
+    let(:heading) { 'Help' }
+    let(:page_title) { 'Help' }
 
-    it { should have_selector('h1', text: 'Help') }
-    it { should have_selector('title', text: full_title('Help')) }
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before { visit about_path }
+    let(:heading) { 'About Us' }
+    let(:page_title) { 'About' }
 
-    it { should have_selector('h1', text: 'About Us') }
-    it { should have_selector('title', text: full_title('About')) }
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
     before { visit contact_path }
+    let(:heading) { 'Contact Us' }
+    let(:page_title) { 'Contact' }
 
-    it { should have_selector('h1', text: 'Contact Us') }
-    it { should have_selector('title', text: full_title('Contact')) }
+    it_should_behave_like "all static pages"
   end
 
   describe "David David Heinemeier Hansson page" do
     before { visit dhh_path }
+    let(:heading) { 'David Heinemeier Hansson' }
+    let(:page_title) { 'DHH' }
 
-    it { should have_selector('h1', text: 'David Heinemeier Hansson') }
-    it { should have_selector('title', text: full_title('DHH')) }
+    it_should_behave_like "all static pages"
   end
 end
